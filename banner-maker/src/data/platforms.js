e3be3bfaeda575e2b353fe1bb4140d0dae6a41bd
LinggarRam <123456789+odin@users.noreply.github.com>
2026-06-23 08:47:07 +0700

    add custom datas

---
+++ banner-maker/src/data/platforms.js
@@ -0,0 +1,25 @@
+export const platforms = [
+  {
+    id: "instagram",
+    name: "Instagram",
+    color: "#e1306c",
+    gradient: "from-purple-500 via-pink-500 to-orange-400",
+    bgLighr: "bg-pink-50",
+    textColor: "text-pink-600",
+    sizes: [
+      {
+        label: "Profile Cover",
+        width: 1080,
+        height: 1920,
+        desc: "Story / Highlight Cover",
+      },
+      { label: "Profile Cover", width: 1080, height: 1080, desc: "Feed Post" },
+      {
+        label: "Profile Cover",
+        width: 1080,
+        height: 566,
+        desc: "Landscape Feed",
+      },
+    ],
+  },
+];
